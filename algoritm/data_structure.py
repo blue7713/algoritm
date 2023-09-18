de05898d4b2eaee385b03c559ae1cd1d52b76557
Ryu hyeon seok <gustjr1058@naver.com>
2023-09-18 23:34:52 +0900

    linked list

--- algoritm/data_structure.py
+++ algoritm/data_structure.py
@@ -12,7 +12,7 @@ class Node:
         object conneted to the next node in a linked list.
 
     """
-    def __init__(self, data : Any - None, next : 'Node' - None) -> None:
+    def __init__(self, data : Any = None, next : 'Node' = None) -> None:
         """
         Args :
             data (Any, optional) : data that user store on Node instance
@@ -22,5 +22,35 @@ class Node:
             None
 
         """
-        self.data = data
-        self.next = None
+        self._data = data # self._ 는 외부에서 접근을 막는 것(information hiding)
+        self._next = None
+
+    @property
+    def data(self):
+        ''' data that user store on Node instance'''
+        return self._data
+    
+    @property
+    def next(self):
+        '''object conneted to the next node int a linked list'''
+        return self._next
+    
+    @data.setter # information hiding을 하면 해주어야하는 정의
+    def data(self, value : Any) -> None:
+        self._data = value
+
+    @next.setter
+    def next(self, node : 'Node') -> None:
+        self._next = node
+
+    def __str__(self) -> str:
+        return_str = f"I love a data : {self._data}\n" \
+                    + f"I love a next node : {id(self._next)}"
+        return return_str
+        
+    def __repr__(self) -> str:
+        return_str = f"Node({self._data})\n"
+        return return_str
+            
+    def __add__(self, other) -> None:
+        self._next = other
--- algoritm/node_implemetation.py
+++ algoritm/node_implemetation.py
@@ -1,4 +1,5 @@
 import data_structure as ds
+from random import *
 # import importlib 
 # importlib.reload(ds) # 수정한 모튤 다시 로드
 
@@ -13,7 +14,23 @@ c = None
 
 print(a.next.data)
 print(a)
+print(b)
+print(c)
+
 print(a.__doc__) # 데이터 타입의 여러 정보 출력
+print(a.__dict__)
+
+node_list = []
+for a in range(1000):
+    node_list.append(ds.Node(randint(1, 10000)))
+
+for idx in range(1, len(node_list)):
+    node_list[idx -1] + node_list[idx]
+
+print(node_list[0])
+print(node_list[:5])
+print(node_list[0].data)
+print(node_list[0].next.data)
 
 # PEP 257: 파이썬의 Docstring 정의 프로포절
 # 클래스, 함수, 변수 등에 대한 정보를 서술함
@@ -24,3 +41,9 @@ print(a.__doc__) # 데이터 타입의 여러 정보 출력
 # Dynamic typing의 파이썬이지만 in/out 파라메터의 타입에 대한 정보를 사전에
 # 정의해서 사용함(Python 3.5이상)
 # 반드시 강제하지 않음 : 내 코드는 나만보지 않는다.
+
+# Special Method for Python Class
+# 파이썬의 Class 정의시 공통적으로 정의되는 method
+# __str__, __init__, __len__, __add__ 등 사전 정의된 함수로 다양한
+# operation과 매칭됨
+# 자신의 class의 특징을 고려하자!
