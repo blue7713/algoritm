45a52827caff02c8ec0a05c4dee3627dc4620d37
Ryu hyeon seok <gustjr1058@naver.com>
2023-09-26 23:27:00 +0900

    lfs test

--- algoritm/linked_list.py
+++ algoritm/linked_list.py
@@ -1,27 +1,3 @@
-import data_structure as ds
-import random
-
-node_list = []
-
-# linked list데이터 생성
-for i in range(5):
-    node_list.append(ds.Node(random.randint(1, 10000)))
-
-print(node_list)
-
-for idx in range(1, len(node_list)): # data와 link부분 연결
-    node_list[idx - 1] + node_list[idx]
-    print(node_list[idx - 1] + node_list[idx])
-
-print(node_list[0].next)
-
-a = node_list[0]
-print(a)
-
-# # treverse(링크작업)
-# cur_node = a
-# while cur_node is not None:
-#     print(cur_node.data)
-#     cur_node = cur_node.next
-# else:
-#     print("End of List")
+version https://git-lfs.github.com/spec/v1
+oid sha256:7ef45edab8aa9683a3519ba68a744bf29c9eb6eb7149bc40aca39c027a82ff80
+size 809
---
+++ algoritm/slide_show.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36918710e03aafa836dd094f5e2d7a587d92183f41b61e6613a2f69e3ca5d775
+size 5878
