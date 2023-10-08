8f290d15acd5682479c8133aec1d8a12c664f18e
Ryu hyeon seok <gustjr1058@naver.com>
2023-10-08 15:08:44 +0900

    algorithm linked list

--- algoritm/slide_show.py
+++ algoritm/slide_show.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b52288948361270cc22c2ec6ca96c9db6ef28abeb455989a4a7cf3326e7a0a41
-size 8876
+oid sha256:0af91cb7d6ec6e0718bba1e8c1113012b45145dfc81d5321d0eceef1593fffef
+size 12602
